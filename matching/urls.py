4d7e0d8c65993c9588122c8992bdb8e2e24006c4
Wudonmat <awzyou70@gmail.com>
2021-11-12 21:21:54 +0900

    matching update

--- matching/urls.py
+++ matching/urls.py
@@ -5,6 +5,7 @@ from matching import views
 app_name = "matching"
 urlpatterns = [
     path("", views.matching_main, name="main"),
+    path("request/", views.request_list, name="request"),
     # path("<int:area_id>/", views.matching_area, name="main"),
     # path("<int:area_id>/<int:user_id>/list", views.matching_main, name="main"),
     # path("<int:blog_id>/", views.shop_blog_home, name="home"),
--- matching/views.py
+++ matching/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def matching_main(request):
     return render(request, "matching.html")
+
+def request_list(request):
+    return render(request, "request_list.html")
